df35a742be5de8bd99f986184b36ea9b4018b86e
Syed Asrar <codecraftbysyed@gmail.com>
2026-06-30 15:15:29 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+  companyName: "Tirupattur Multi Print Pvt Ltd",
+  whatsappNumber: "919443929962",
+  phoneNumber: "+91 94439 29962",
+  email: "tptmultiprint@gmail.com",
+  address: "#106 / 5, Cutchery Street, Tirupattur - 635 601",
+  factoryAddress: "Avvai Nagar, Tirupattur - 635 601",
+  website: "https://tirupatturmultiprint.com",
+  registrationNumber: "U22219TN2020PTC139059",
+  directorName: "P.R. Murugesan",
+  otherPhoneNumbers: ["+91 94439 29962", "+91 95785 35324"],
+  otherEmails: ["tmpdigitalprint@gmail.com", "tmpoffsetprint@gmail.com"],
+  // Use only the embed URL (not the full iframe tag). Example: https://www.google.com/maps/embed?pb=...
+  mapEmbedUrl: "https://maps.google.com/maps?q=12.489212587783555,78.54907567506834&z=17&output=embed"
+};
+
+window.CONFIG = CONFIG;
